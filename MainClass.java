2e20d77a295d3b19765ea46d1c203a14ad42bee3
Lars Kellogg-Stedman <lars@oddbit.com>
2024-01-21 19:00:06 -0500

    Update to match requirements

--- MainClass.java
+++ MainClass.java
@@ -3,36 +3,66 @@ package Pck1;
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args) {
-        Scanner input =  new Scanner(System.in);
-        System.out.print("Enter initial speed between 0 and 6: ");
-        int speed = input.nextInt();
+    /**
+     * Prompt the user for a new Throttle position. This routine will
+     * loop until the user enters valid data, re-prompting the user each
+     * time.
+     */
+    private static int readPosition(Scanner input) {
+        int position;
 
-        Throttle throttle = new Throttle(speed);
-
-mainloop:
-        while(throttle.is_on()) {
-            int shift;
-
-            System.out.println("Flow is now: " + throttle.flow());
-            while (true) {
-                System.out.print("Enter 'u' to shift up, 'd' to shift down, 'q' to quit: ");
-                String answer = input.next();
-
-                if (answer.equals("u")) {
-                    shift = 1;
+        // Keep prompting until we get valid input.
+        while (true) {
+            System.out.print("Enter throttle position (0-6); enter -1 to quit: ");
+            try {
+                position = input.nextInt();
+                if (position >= -1 && position <= 6)
+                    // Exit the loop if we have valid data.
                     break;
-                } else if (answer.equals("d")) {
-                    shift = -1;
-                    break;
-                } else if (answer.equals("q")) {
-                    break mainloop;
-                } else {
-                    System.out.println("Invalid input.");
-                }
+            } catch (java.util.InputMismatchException e) {
+                /**
+                 * We end up here if the user enters a non-integer response.
+                 * This call discards the invalid token so that we can try
+                 * reading new input.
+                 */
+                input.next();
             }
 
-            throttle.shift(shift);
+            /**
+             * If we get here, the user entered invalid data. Either they entered
+             * a number that it out of range, or they entered some non-numeric
+             * response. In either case, display an error and re-prompt.
+             */
+            System.out.println("Invalid input.");
+        }
+
+        return position;
+    }
+
+    public static void main(String[] args) {
+        Throttle throttle;
+
+        /**
+         * This is a try-with-resources [1] statement; it ensures that we properly
+         * close the scanner when we exit the try block. That's not strictly necessary
+         * in this code, but it stops my IDE from complaining about the unclosed scanner.
+         *
+         * [1]: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+         */
+        try (Scanner input = new Scanner(System.in)) {
+            while(true) {
+                int position = readPosition(input);
+
+                if (position == -1) {
+                    // Exit if user enters -1.
+                    break;
+                } else {
+                    // Otherwise create a new Throttle with updated position.
+                    throttle = new Throttle(position);
+                }
+
+                System.out.println("Flow is now: " + throttle.flow());
+            }
         }
     }
 }
